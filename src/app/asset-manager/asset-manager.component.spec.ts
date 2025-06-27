8f321f562c9bdd4472151ab276c4d6f33206d1f3
Jarod <razeixyz@gmail.com>
2025-06-27 17:48:34 -0400

    add rendering logic for AddAssetModalComponent in AssetManagerComponent and update tests

--- src/app/asset-manager/asset-manager.component.spec.ts
+++ src/app/asset-manager/asset-manager.component.spec.ts
@@ -1,18 +1,51 @@
-import { TestBed, waitForAsync } from '@angular/core/testing'; 
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AssetManagerComponent } from './asset-manager.component';
+import { MockComponent } from 'ng-mocks';
+import { AddAssetModalComponent } from '../add-asset-modal/add-asset-modal.component';
 
-describe('AssetManagerComponent', () => { 
-  beforeEach(waitForAsync(() => { 
+describe('AssetManagerComponent', () => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        AssetManagerComponent
-      ],
+      imports: [AssetManagerComponent, MockComponent(AddAssetModalComponent)],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', async () => {
     const fixture = TestBed.createComponent(AssetManagerComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.debugElement.componentInstance;
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component).toBeTruthy();
+  });
+
+  it('should render AddAssetModalComponent when showModal is true', async () => {
+    const fixture = TestBed.createComponent(AssetManagerComponent);
+    const component = fixture.debugElement.componentInstance;
+    const container = fixture.debugElement.nativeElement;
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    component.showModal.set(true);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(container.querySelector('app-add-asset-modal')).toBeTruthy();
+  });
+
+  it('should not render AddAssetModalComponent when showModal is false', async () => {
+    const fixture = TestBed.createComponent(AssetManagerComponent);
+    const component = fixture.debugElement.componentInstance;
+    const container = fixture.debugElement.nativeElement;
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    component.showModal.set(false);
+
+    expect(container.querySelector('app-add-asset-modal')).toBeFalsy();
   });
 });
