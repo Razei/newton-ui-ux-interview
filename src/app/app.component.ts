14c044fb753de392923297a79a09f1a7a155f8a1
Jarod <razeixyz@gmail.com>
2025-06-27 18:34:10 -0400

    add LandingPageComponent to AppComponent and update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
+  imports: [LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
