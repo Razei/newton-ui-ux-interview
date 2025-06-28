6fa93824ad0b87a1b6a3346a34ba314a26eed25a
Jarod <razeixyz@gmail.com>
2025-06-28 16:15:38 -0400

    Enhance asset table component: improve form layout, add currency masking, and implement asset type selection

--- src/app/assets/components/asset-table/asset-table.component.ts
+++ src/app/assets/components/asset-table/asset-table.component.ts
@@ -12,19 +12,20 @@ import {
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { faCirclePlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { AssetStore } from '../../store/asset.store';
 import { createNewAssetFormGroup } from '../../utils/asset.utils';
 import {
   AssetFormArray,
   AssetFormGroup,
 } from '../add-asset-modal/add-asset-modal.component';
-
 @Component({
   selector: 'app-asset-table',
-  imports: [ReactiveFormsModule, FaIconComponent],
+  imports: [ReactiveFormsModule, FaIconComponent, NgxMaskDirective],
   templateUrl: './asset-table.component.html',
   styleUrl: './asset-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNgxMask()],
 })
 export class AssetTableComponent {
   // #region Dependencies
