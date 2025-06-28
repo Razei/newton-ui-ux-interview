804a0ee21e613b3d87f99900d89be7d5f454ea66
Jarod <razeixyz@gmail.com>
2025-06-27 20:34:34 -0400

    Refactor asset manager component: update layout and integrate modal service for adding assets

--- src/app/asset-manager/asset-manager.component.ts
+++ src/app/asset-manager/asset-manager.component.ts
@@ -1,21 +1,34 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import { AddAssetModalComponent } from '../add-asset-modal/add-asset-modal.component';
 import {
   FaIconComponent,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-asset-manager',
-  imports: [AddAssetModalComponent, FaIconComponent],
+  imports: [FaIconComponent],
   templateUrl: './asset-manager.component.html',
   styleUrl: './asset-manager.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssetManagerComponent {
-  readonly showModal = signal(false);
+  // #region Dependencies
+  readonly modalService = inject(NgbModal);
+  // #endregion
 
   constructor(library: FaIconLibrary) {
     library.addIcons(faHandHoldingDollar);
+  }
+
+  whenAddAssetsButtonClicked() {
+    this.modalService.open(AddAssetModalComponent, { scrollable: true });
   }
 }
