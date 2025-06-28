8681dd260178bac35b095854951549b3309057ce
Jarod <razeixyz@gmail.com>
2025-06-28 13:56:32 -0400

    Implement asset form handling in AddAssetModal; add createNewAssetFormGroup utility for asset creation

--- src/app/assets/components/add-asset-modal/add-asset-modal.component.ts
+++ src/app/assets/components/add-asset-modal/add-asset-modal.component.ts
@@ -1,15 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import {
   FaIconComponent,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Nullable } from '../../../utils/typescript.utils';
 import { AssetStore } from '../../store/asset.store';
+import { createNewAssetFormGroup } from '../../utils/asset.utils';
+import { AssetTableComponent } from '../asset-table/asset-table.component';
 
 @Component({
   selector: 'app-add-asset-modal',
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, AssetTableComponent],
   templateUrl: './add-asset-modal.component.html',
   styleUrl: './add-asset-modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,8 +21,39 @@ import { AssetStore } from '../../store/asset.store';
 export class AddAssetModalComponent {
   readonly activeModal = inject(NgbActiveModal);
   readonly assetStore = inject(AssetStore);
+  readonly assetForm = new FormGroup<AssetFormArray>({
+    assets: new FormArray<AssetFormGroup>([createNewAssetFormGroup()]),
+  });
 
   constructor(library: FaIconLibrary) {
     library.addIcons(faHandHoldingDollar);
   }
+
+  saveAssets() {
+    if (this.assetForm.invalid) {
+      this.assetForm.markAllAsTouched();
+      return;
+    }
+
+    const value = this.assetForm.getRawValue();
+
+    this.assetStore.upsertAssets(value.assets);
+
+    this.activeModal.close('Close click');
+  }
 }
+
+export type AssetFormArray = {
+  assets: FormArray<AssetFormGroup>;
+};
+
+export type AssetFormGroup = FormGroup<AssetControls>;
+
+export type AssetControls = {
+  /**
+   * Since these are FE only assets, we can use a generated UUID as the ID.
+   */
+  id: FormControl<Nullable<string>>;
+  type: FormControl<string>;
+  value: FormControl<number | null>;
+};
---
+++ src/app/assets/utils/asset.utils.ts
@@ -0,0 +1,15 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AssetControls } from '../components/add-asset-modal/add-asset-modal.component';
+
+export const createNewAssetFormGroup = () => {
+  return new FormGroup<AssetControls>({
+    id: new FormControl(null),
+    type: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    value: new FormControl(null, {
+      validators: [Validators.required],
+    }),
+  });
+};
