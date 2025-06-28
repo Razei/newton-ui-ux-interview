7eadc806fb392988acfc6314505a0e3c213c2053
Jarod <razeixyz@gmail.com>
2025-06-27 21:08:36 -0400

    Add mock providers for NgbActiveModal and AssetStore in AddAssetModalComponent tests

--- src/app/add-asset-modal/add-asset-modal.component.spec.ts
+++ src/app/add-asset-modal/add-asset-modal.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AddAssetModalComponent } from './add-asset-modal.component';
+import { MockProvider } from 'ng-mocks';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AssetStore } from '../assets/asset.store';
 
 describe('AddAssetModalComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [AddAssetModalComponent],
+      providers: [MockProvider(NgbActiveModal), MockProvider(AssetStore)],
     }).compileComponents();
   }));
 
