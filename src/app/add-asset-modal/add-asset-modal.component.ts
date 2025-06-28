4ccb211fce34d9f13d967a38d890867a3e682ecb
Jarod <razeixyz@gmail.com>
2025-06-27 20:34:20 -0400

    Implement AddAssetModalComponent with header, body, and footer structure

--- src/app/add-asset-modal/add-asset-modal.component.ts
+++ src/app/add-asset-modal/add-asset-modal.component.ts
@@ -1,10 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-asset-modal',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './add-asset-modal.component.html',
   styleUrl: './add-asset-modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddAssetModalComponent {}
+export class AddAssetModalComponent {
+  readonly activeModal = inject(NgbActiveModal);
+
+  readonly name = input('World');
+}
